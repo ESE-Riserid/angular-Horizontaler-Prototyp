387f962a8bc7118c3d18d29f68040ee21748ca46
ESE-Riserid <121957717+ESE-Riserid@users.noreply.github.com>
2023-01-05 16:04:15 +0100

    3 files modified

--- src/app/menu-nested-example.ts
+++ src/app/menu-nested-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 /**
  * @title Nested menu
  */
@@ -8,13 +8,40 @@ import {Component} from '@angular/core';
   templateUrl: 'menu-nested-example.html',
 })
 export class MenuNestedExample {
-
   toDisplay = false;
+  textOn = true;
+  feld1 = 'hallo';
+  formfield = {
+    field: '',
+  };
+  form: FormGroup;
+  fieldValue: string = '';
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      field: new FormControl(this.formfield.field, []),
+    });
+  }
+
+  fill1() {
+    this.form.setValue({
+      field: '1.Wert',
+    });
+  }
+
+  fill2() {
+    this.form.setValue({
+      field: '2.Wert',
+    });
+  }
+  toggle() {
+    this.textOn = !this.textOn;
+  }
+
   toggleData() {
     this.toDisplay = !this.toDisplay;
   }
 }
-
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
