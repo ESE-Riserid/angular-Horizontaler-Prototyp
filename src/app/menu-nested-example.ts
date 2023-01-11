75a60b5c6c8fe889c4c3c9b6bc7f572db7d92207
ESE-Riserid <121957717+ESE-Riserid@users.noreply.github.com>
2023-01-11 15:37:29 +0100

    2 files modified, 2 added

--- src/app/menu-nested-example.ts
+++ src/app/menu-nested-example.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
 /**
  * @title Nested menu
  */
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
 @Component({
   selector: 'menu-nested-example',
   templateUrl: 'menu-nested-example.html',
@@ -11,11 +24,43 @@ export class MenuNestedExample {
   toDisplay = false;
   textOn = true;
   feld1 = 'hallo';
+  favoriteSeason: string;
+  seasons: string[] = ['1.Wert', '2.Wert', '3.Wert', '4.Wert'];
   formfield = {
     field: '',
   };
+  checked = false;
+  indeterminate = false;
+  labelPosition: 'before' | 'after' = 'after';
+  disabled = false;
   form: FormGroup;
   fieldValue: string = '';
+  disableSelect = new FormControl(false);
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { name: this.name, animal: this.animal },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+  openKunden(): void {
+    const dialogRef = this.dialog.open(KundenExample, {
+      data: { name: this.name, animal: this.animal },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -40,6 +85,34 @@ export class MenuNestedExample {
 
   toggleData() {
     this.toDisplay = !this.toDisplay;
+  }
+}
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-example.html',
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+@Component({
+  selector: 'kunden-overview-example-dialog',
+  templateUrl: 'Kunden-example.html',
+})
+export class KundenExample {
+  constructor(
+    public dialogRef: MatDialogRef<KundenExample>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
 
